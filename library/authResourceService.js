a0838acda4c741ec0bcf0cce039eecd41d6f8fbf
Sy Le <lenguyensy@gmail.com>
2018-05-24 07:25:25 -0700

    wip google oauth

--- library/authResourceService.js
+++ library/authResourceService.js
@@ -1,13 +1,16 @@
 var authDataService = require('./authDataService');
 
 var AuthResourceService = {
-  createUserLocal: function(email, password){
+  createUserLocal: function(email, password) {
     return authDataService.UserLocal.create({
       email: email,
       password: password
     });
   },
-  findLocalUser: function(email){
+  createUserGmail: function(accessToken, refreshToken, profile) {
+    return Promise.resolve();
+  },
+  findLocalUser: function(email) {
     return authDataService.UserLocal.findAll({
       where: { email: email }
     });
